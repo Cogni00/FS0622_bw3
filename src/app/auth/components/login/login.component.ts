2a5edfbadc318c7fd42ede8e081038fede444866
Lorenzo De Carolis <lorenzodc@hotmail.itm>
2022-12-14 23:44:31 +0100

    fix funzioni login/register e aggiunta gestione errori

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { catchError } from 'rxjs';
 import { AuthService } from '../../auth.service';
 
 @Component({
@@ -10,16 +11,27 @@ import { AuthService } from '../../auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private authSrv:AuthService, private router:Router) { }
+
+  err: string | undefined
+  constructor(private authSrv: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   async onsubmit(form: NgForm) {
     console.log(form.value);
-      await this.authSrv.login(form.value).toPromise();
-      this.router.navigate(['/'])
-      // da decidere dove punta una vlota fatto login['/']
+    await this.authSrv.login(form.value).pipe(catchError(err => {
+      if (err.error == "Cannot find user") {
+        this.err = `Utente non registrato`
+      } else if (err.error == "Incorrect password") {
+        this.err = `Password errata`
+      } else if (err.error == "Email format is invalid") {
+        this.err = `Formato email errato`
+      }
+      throw err
+    })).toPromise();
+    this.router.navigate(['/'])
+    // da decidere dove punta una vlota fatto login['/']
   }
 
 }
